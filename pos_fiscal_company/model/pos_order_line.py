1df394e124e67a843217ecb0a360b8f18267434e
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-02-07 12:58:42 +0100

    [PORT] pos_fiscal_company

--- pos_fiscal_company/model/pos_order_line.py
+++ pos_fiscal_company/model/pos_order_line.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+# flake8: noqa
 ##############################################################################
 #
 #    Fiscal Company for Point Of Sale Module for Odoo
@@ -28,28 +29,28 @@ from openerp.osv import fields
 class pos_order_line(Model):
     _inherit = 'pos.order.line'
 
+    # Override Section
     def _amount_line_all(self, cr, uid, ids, field_names, arg, context=None):
         res = dict([(i, {}) for i in ids])
         account_tax_obj = self.pool.get('account.tax')
         cur_obj = self.pool.get('res.currency')
         for line in self.browse(cr, uid, ids, context=context):
-            taxes_ids = [
-                tax for tax in line.product_id.taxes_id if (
+            # <modified code>
+            # Includes taxes of the parent company
+            taxes_ids = [ tax for tax in line.product_id.taxes_id if (
                     tax.company_id.id == line.order_id.company_id.id or
                     (tax.company_id.id ==
                         line.order_id.company_id.fiscal_company.id))]
+            # </modified code>
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            taxes = account_tax_obj.compute_all(
-                cr, uid, taxes_ids, price, line.qty, product=line.product_id,
-                partner=line.order_id.partner_id or False)
+            taxes = account_tax_obj.compute_all(cr, uid, taxes_ids, price, line.qty, product=line.product_id, partner=line.order_id.partner_id or False)
 
             cur = line.order_id.pricelist_id.currency_id
-            res[line.id]['price_subtotal'] = cur_obj.round(
-                cr, uid, cur, taxes['total'])
-            res[line.id]['price_subtotal_incl'] = cur_obj.round(
-                cr, uid, cur, taxes['total_included'])
+            res[line.id]['price_subtotal'] = taxes['total']
+            res[line.id]['price_subtotal_incl'] = taxes['total_included']
         return res
 
+    # Column Section
     _columns = {
         'price_subtotal': fields.function(
             _amount_line_all, multi='pos_order_line_amount',
